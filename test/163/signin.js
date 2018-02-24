0db48d914249b51a3ced8c2f82eb18541609fce9
hengwu <youchuangqiba@163.com>
2018-02-25 02:06:24 +0800

    upd8

--- test/163/signin.js
+++ test/163/signin.js
@@ -1,37 +1,28 @@
-const puppeteer = require('puppeteer');
-
 const { wait } = require('../util');
 
-module.exports = async ({
-    username,
-    password
-}, puppeteerConf = {}) => {
-    puppeteer.launch(puppeteerConf).then(async browser => {
-        const page = await browser.newPage();
-        await page.goto('https://mail.163.com/');
+module.exports = async ({ username, password }, browser) => {
+    const page = await browser.newPage();
+    await page.goto('https://mail.163.com/');
 
-        await wait(1000);
+    await wait(1000);
 
-        await page.waitForSelector('#x-URS-iframe');
-        let frames = await page.frames();
-        const frame = frames[3];
-        const frameContext = await frame.executionContext();
+    await page.waitForSelector('#x-URS-iframe');
+    let frames = await page.frames();
+    const frame = frames[3];
+    const frameContext = await frame.executionContext();
 
-        const usernameSelector = '#login-form > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(2) > input:nth-of-type(1)';
-        const passwordSelector = '#login-form > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(2) > input:nth-of-type(2)';
+    const usernameSelector = '#login-form > div:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(2) > input:nth-of-type(1)';
+    const passwordSelector = '#login-form > div:nth-of-type(1) > div:nth-of-type(3) > div:nth-of-type(2) > input:nth-of-type(2)';
 
-        await frameContext.evaluate(`document.body.querySelector('${usernameSelector}').value = '${username}'`);
-        await frameContext.evaluate(`document.body.querySelector('${passwordSelector}').value = '${password}'`);
-        await frameContext.evaluate(`document.body.querySelector('#dologin').click()`);
+    await frameContext.evaluate(`document.querySelector('${usernameSelector}').value = '${username}'`);
+    await frameContext.evaluate(`document.querySelector('${passwordSelector}').value = '${password}'`);
+    await frameContext.evaluate(`document.querySelector('#dologin').click()`);
 
-        await page.waitForNavigation();
-        await page.waitForSelector('.mboxlst');
-        const result = await page.evaluate(_ => {
-            let q = document.body.querySelectorAll('.mboxlst');
-            return q[q.length - 1].innerText;
-        }, 7);
-        console.log(result);
-
-        await browser.close();
-    });
+    await page.waitForNavigation();
+    await page.waitForSelector('.mboxlst');
+    const result = await page.evaluate(_ => {
+        let q = document.querySelectorAll('.mboxlst');
+        return q[q.length - 1].innerText;
+    }, 7);
+    console.log(result);
 };
---
+++ test/github/signin.js
@@ -0,0 +1,19 @@
+const { wait } = require('../util');
+
+module.exports = async ({ username, password }, browser) => {
+    const page = await browser.newPage();
+    await page.goto('https://github.com/login');
+
+    await wait(1000);
+
+    const usernameSelector = 'input#login_field';
+    const passwordSelector = 'input#password';
+
+    await page.type(usernameSelector, username);
+    await page.type(passwordSelector, password);
+    await page.click('#login form > div:last-child input:last-child');
+
+    await page.waitForNavigation();
+    const result = await page.$eval('ul.mini-repo-list', e => e.innerText);
+    console.log(result);
+};
