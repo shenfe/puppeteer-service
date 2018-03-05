5ba8ff5762a12344543c6dbc7ccd3052e708a06f
hengwu <youchuangqiba@163.com>
2018-03-06 03:15:28 +0800

    upd8

--- test/batch.js
+++ test/batch.js
@@ -6,7 +6,7 @@ const { port, apiName } = require('../src/config').server;
 
 const batchPromises = [];
 
-const time = process.hrtime();
+console.time('time consumed: ');
 
 for (let i = 0; i < 100; i++) {
     let p = Run(`http://127.0.0.1:${port}/${apiName}?q=${i}`, {
@@ -35,5 +35,5 @@ for (let i = 0; i < 100; i++) {
 
 Promise.all(batchPromises).then(() => {
     const diff = process.hrtime(time);
-    console.log('took %d ms', Math.ceil((diff[0] * 1e9 + diff[1]) / 1000000));
+    console.timeEnd('time consumed: ');
 });
